faa9acfcf5a602e52747bd730a2a83a2a5a6c262
jeanpah <jeanpah@hotmail.com>
2017-05-23 15:04:14 -0500

    cargue de archivos listo, filtrar por tipo de archivo tambien

--- js/uploadQuestionary.js
+++ js/uploadQuestionary.js
@@ -7,13 +7,31 @@ cont_angular.controller('uploadQuestionaryCtrl', ['$scope', '$stateParams', '$io
           console.log("electron caragado")
     			const dialog = electron.dialog;
           console.log("dialog caragado")
-          dialog.showOpenDialog(function (fileNames) {
+          dialog.showOpenDialog({filters:[{name: 'Prueba tipo saber', extensions: ['prueba']}]}, function (fileNames) {
             if (fileNames === undefined) return;
             var fileName = fileNames[0];
             $scope.data.path = fileName;
-            console.log($scope.data.path)
+            $scope.$apply()
+            //console.log($scope.data.path)
           });
-          this.value = $scope.data.path
+          //document.getElementById("filepathInput").value = $scope.data.path
         };
+
+        $scope.loadFile=function(){
+          var fs = nodeRequire('fs');
+          fs.readFile($scope.data.path, 'utf8', function (err, data) {
+            if (err) return console.log(err);
+            lines = data.split('\n');
+            var nombreprueba = lines[0];
+            var preguntas = lines[1].split(',')
+            console.log(nombreprueba)
+            console.log(preguntas);
+            // data is the contents of the text file we just read
+          });
+        }
+
+        $scope.cancel=function(){
+          $state.go("start")
+        }
 
     }]);
